57b8baba4b46f145668ea622fbf60de178cf4414
Duncan Doyle <Duncan.Doyle@gmail.com>
2019-11-08 15:46:05 +0100

    Fix missing RHPAM version number in PowerShell scripts.

--- support/openshift/provision-properties-static.ps1
+++ support/openshift/provision-properties-static.ps1
@@ -4,4 +4,4 @@ $PRJ_DESCRIPTION="Red Hat Process Automation Manager 7 Order IT Hardware Demo"
 #GitHub tag referencing the image streams and templates.
 $OPENSHIFT_PAM7_TEMPLATES_TAG="7.5.0.GA"
 $IMAGE_STREAM_TAG="7.5.0"
-$PAM7_VERSION
+$PAM7_VERSION="75"
